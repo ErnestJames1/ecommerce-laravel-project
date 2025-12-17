536f48dcaa48912c4c93f299cd9be9c912ce05c8
Ernest James <ernestjames193@gmail.com>
2025-12-18 02:58:40 +0800

    Chapter 24

--- app/Livewire/MyOrdersPage.php
+++ app/Livewire/MyOrdersPage.php
@@ -3,11 +3,23 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\Title;
 
+
+#[Title('My Orders')]
 class MyOrdersPage extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.my-orders-page');
+
+        $my_orders = Order::where('user_id', Auth::id())->latest()->paginate(5);
+        return view('livewire.my-orders-page', [
+            'orders' => $my_orders,
+        ]);
     }
 }
